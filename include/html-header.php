0a701728e4629c16f4b7a8729d6ef46d8a183113
Silvio Rönick <silvio.roenick@outlook.de>
2017-12-11 16:03:50 +0100

    ideen sammeln
    aufbau plannen

---
+++ include/html-header.php
@@ -0,0 +1,11 @@
+<?php
+
+	echo '<head>';
+	echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+	echo '<link rel="stylesheet" type="text/css" href="elements/main.css">';
+	echo '<title></title>';
+	//echo '';
+	echo '<script type="text/javascript" src="elements/main.js"></script>';
+	echo '</head>';
+	
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include ("config.php");
+
+echo '<!DOCTYPE html>';
+echo '<html>';
+
+include ("include/html-header.php");
+
+echo '<body>';
+
+echo 'Hallo Welt!';
+
+echo '</body>';
+echo '</html> ';
+
+?>
